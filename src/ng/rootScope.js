9ba71c34d24c76f9422972ee1900f142ef9a471b
Tero Parviainen <tero.parviainen@eficode.fi>
2013-11-03 20:48:57 +0200

    Keep digesting while dirty

--- src/ng/rootScope.js
+++ src/ng/rootScope.js
@@ -10,15 +10,24 @@ Scope.prototype.$watch = function(watchFn, listenerFn) {
   this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function() {
-  var self = this;
+Scope.prototype.$$digestOnce = function() {
+  var self  = this;
+  var dirty;
   _.forEach(this.$$watchers, function(watch) {
     var newValue = watch.watchFn(self);
     var oldValue = watch.last;
     if (newValue !== oldValue) {
       watch.listenerFn(newValue, oldValue, self);
+      dirty = true;
     }
     watch.last = newValue;
-  });  
+  });
+  return dirty;
 };
 
+Scope.prototype.$digest = function() {
+  var dirty;
+  do {
+    dirty = this.$$digestOnce();
+  } while (dirty);
+};
